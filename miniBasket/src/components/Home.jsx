b74185f66e6176ecbb6304bfe9397cf950d33777
githubjyo <ghugejyoti7@gmail.com>
2025-08-25 14:58:02 +0530

    add-viewproduct-button

--- miniBasket/src/components/Home.jsx
+++ miniBasket/src/components/Home.jsx
@@ -1,6 +1,14 @@
-export default function Home() {
-    return(
+import { useNavigate } from "react-router-dom"
 
-        <h1>Home</h1>
+
+export default function Home() {
+   const nav = useNavigate()
+    return(
+         <>
+
+          <div className="d-flex justify-content-center align-items-center">
+            <button className="btn btn-primary mx-5 mt-5" onClick={() => nav('/product')}>View Product</button>
+            </div>
+          </>
     )
 }
